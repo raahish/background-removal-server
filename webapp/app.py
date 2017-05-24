ff88760981e390f6f509065552e04e529c44a734
Alon Burg <burgalon@gmail.com>
2017-05-24 18:30:14 +0300

    initial commit

---
+++ webapp/app.py
@@ -0,0 +1,60 @@
+import base64
+
+import io
+from flask import Flask, jsonify, request
+import keras
+from PIL import Image
+from flask import send_file
+from io import BytesIO
+from keras.models import model_from_json
+import numpy as np
+from scipy.misc import imresize, imsave
+import tensorflow as tf
+
+json_file = open('./model/model.json', 'r')
+model = model_from_json(json_file.read())
+json_file.close()
+model.load_weights('./model/weights224.01-4.25.hdf5', by_name=True)
+graph = tf.get_default_graph()
+
+app = Flask(__name__)
+
+
+def parse_code(l):
+    splitted = l.strip().split("\t")
+    a = splitted[0]
+    b = splitted[-1]
+    return tuple(int(o) for o in a.split(' ')), b
+
+label_codes,label_names = zip(*[
+    parse_code(l) for l in open("./label_colors.txt")])
+
+def color_label(a):
+    r,c=a.shape
+    res = np.zeros((r,c,3), 'uint8')
+    for j in range(r):
+        for k in range(c):
+            o=label_codes[a[j,k]]
+            res[j,k] = o
+    return res
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # The current image from the center camera of the car
+    img_stream = request.files['image']
+    image = Image.open(img_stream)
+    image = imresize(image, (224, 224 ,3))
+    image_array = np.asarray(image)
+    with graph.as_default():
+        prediction = model.predict(image_array[None, :, :, :], batch_size=1)
+        prediction = np.argmax(prediction, axis=-1)
+        prediction = prediction.reshape((-1,224,224))
+
+        mask = prediction[0]
+        imsave('out.jpg', color_label(mask))
+        return send_file(prediction[0])
+        # return jsonify({'prediction': repr(prediction)})
+    # return jsonify({'prediction': None})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
