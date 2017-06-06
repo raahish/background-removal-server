69a33185472a91c9a009104461864ae6ca793cbd
Alon Burg <burgalon@gmail.com>
2017-06-06 13:48:17 +0300

    use H5 instead of JSON

--- webapp/ml.py
+++ webapp/ml.py
@@ -1,11 +1,8 @@
-from keras.models import model_from_json
+from keras.models import load_model
 import numpy as np
 import tensorflow as tf
 
-json_file = open('./model/tiramisu_2_classes.json', 'r')
-model = model_from_json(json_file.read())
-json_file.close()
-model.load_weights('./model/weights224.02-0.28.hdf5', by_name=True)
+model = load_model('tiramisu_2_classes_with_weights.h5')
 graph = tf.get_default_graph()
 
 
