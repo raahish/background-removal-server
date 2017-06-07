073d734156d4ac9382cc6ef00ea547126d62c3c6
Alon Burg <burgalon@gmail.com>
2017-06-07 22:38:16 +0300

    error notifications

--- webapp/static/application.js
+++ webapp/static/application.js
@@ -7,7 +7,6 @@ var dropzone = new Dropzone('form', {
   maxFilesize: 3,
   filesizeBase: 1000,
   sending: function(file, xhr) {
-    console.log('sending', xhr);
     xhr.responseType = 'arraybuffer';
     xhr.responseText = null
   },
@@ -26,7 +25,15 @@ var dropzone = new Dropzone('form', {
 
 });
 
-dropzone.on("complete", function(file) {
+dropzone.on("error", function(file) {
+  $('.alert-danger.hidden')
+    .clone()
+    .appendTo('.results')
+    .removeClass('hidden')
+    .append(file.name)
+});
+
+dropzone.on("success", function(file) {
   var blb = new Blob([file.xhr.response], {type: 'image/png'});
   var url = window.url = (window.URL || window.webkitURL).createObjectURL(blb);
   var img = new Image();
@@ -34,45 +41,3 @@ dropzone.on("complete", function(file) {
   $('.results').append(img)
 
 });
-
-
-// Now fake the file upload, since GitHub does not handle file uploads
-// and returns a 404
-
-// var minSteps = 6,
-//   maxSteps = 60,
-//   timeBetweenSteps = 100,
-//   bytesPerStep = 100000;
-//
-// dropzone.uploadFiles = function(files) {
-//   var self = this;
-//
-//   for (var i = 0; i < files.length; i++) {
-//
-//     var file = files[i];
-//     totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
-//
-//     for (var step = 0; step < totalSteps; step++) {
-//       var duration = timeBetweenSteps * (step + 1);
-//       setTimeout(function(file, totalSteps, step) {
-//         return function() {
-//           file.upload = {
-//             progress: 100 * (step + 1) / totalSteps,
-//             total: file.size,
-//             bytesSent: (step + 1) * file.size / totalSteps
-//           };
-//
-//           self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
-//           if (file.upload.progress == 100) {
-//             file.status = Dropzone.SUCCESS;
-//             self.emit("success", file, 'success', null);
-//             self.emit("complete", file);
-//             self.processQueue();
-//           }
-//         };
-//       }(file, totalSteps, step), duration);
-//     }
-//   }
-// }
-//
-// }
