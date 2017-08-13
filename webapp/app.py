f52b6636622c25643f36c6c646fbf6e868bec8b9
Alon Burg <burgalon@gmail.com>
2017-08-13 11:37:27 +0300

    try to fix

--- webapp/app.py
+++ webapp/app.py
@@ -56,11 +56,12 @@ def predict():
     image = request.files['file']
     image = Image.open(image)
     image = rotate_by_exif(image)
-    resized_image = imresize(image, (224, 224)) / 255
+    resized_image = imresize(image, (224, 224)) / 255.0
 
     # Model input shape = (224,224,3)
     # [0:3] - Take only the first 3 RGB channels and drop ALPHA 4th channel in case this is a PNG
     prediction = ml_predict(resized_image[:, :, 0:3])
+    print('PREDICTION COUNT', (prediction[:, :, 1]>0.5).sum())
 
     # Resize back to original image size
     # [:, :, 1] = Take predicted class 1 - currently in our model = Person class. Class 0 = Background
